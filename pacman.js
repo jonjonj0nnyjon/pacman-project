75f5924f1eabcb2d397eb004ec3c06b1413f13c5
Jonathan Tsamalidis <onidelta@Jons-MacBook-Pro.local>
2023-05-07 12:56:50 -0600

    working on collision

--- pacman.js
+++ pacman.js
@@ -5,6 +5,7 @@ class Pacman {
         this.width = width;
         this.height = height;
         this.speed = speed;
+        this.direction = directionRight;
 
     };
 
@@ -22,15 +23,51 @@ class Pacman {
     };
 
     moveBackwards() {
+        switch(this.direction) {
+            case directionRight:
+                this.x -= this.speed
+                break
+            case directionUp:
+                this.y += this.speed
+                break
+            case directionLeft:
+                this.x += this.speed
+                break
+            case directionDown:
+                this.y -= this.speed
+                break
+        }
 
     };
 
     moveForwards() {
+        switch(this.direction) {
+            case directionRight:
+                this.x += this.speed
+                break
+            case directionUp:
+                this.y -= this.speed
+                break
+            case directionLeft:
+                this.x -= this.speed
+                break
+            case directionDown:
+                this.y += this.speed
+                break
+        }
 
     };
 
     checkCollision() {
-
+        if (
+            map[this.getMapY()][this.getMapX()] == 1 ||
+            map[this.getMapYRightSide()][this.getMapX()] == 1 ||
+            map[this.getMapY()][this.getMapXRightSide()] == 1 ||
+            map[this.getMapYRightSide()][this.getMapXRightSide()] == 1
+        ) {
+            return true;
+        };
+        return false;
     };
 
     checkGhostCollision() {
@@ -47,5 +84,21 @@ class Pacman {
 
     draw() {
 
+    };
+
+    getMapX() {
+        return parseInt(this.x / oneBlockSize)
+    };
+
+    getMapY() {
+        return parseInt(this.y / oneBlockSize)
+    };
+    
+    getMapXRightSide() {
+        return parseInt((this.x + 0.9999 * oneBlockSize) / oneBlockSize)
+    };
+    
+    getMapYRightSide() {
+        return parseInt((this.y + 0.9999 * oneBlockSize) / oneBlockSize)
     };
 }
